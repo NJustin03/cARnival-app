5eb921bd3d220c69d961d1132901b17841926344
SirDerpsAlot <57239131+SirDerpsAlot@users.noreply.github.com>
2024-05-21 11:08:05 -0400

    Created Text Prefeb
    Created prefab for ELLE Text.

---
+++ cARnival-Project/Assets/Scripts/Prefab scipts/Text Prefab Script.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextPrefabScript : MonoBehaviour
+{
+    public string Text;
+    public Sprite BackgroundImage = null;
+
+    private void OnValidate()
+    {
+       var TMPObject = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+       var BackImg = gameObject.GetComponentInChildren<Image>();
+
+        TMPObject.text = Text;
+        if (BackgroundImage == null)
+        {
+            BackImg.color = Color.clear;
+        }
+        else
+        {
+            BackImg.color = Color.white;
+        }
+
+        BackImg.sprite = BackgroundImage;
+    }
+
+    [MenuItem("GameObject/ELLE/Text", false)]
+    private static void Create(MenuCommand menuCommand)
+    {
+        var uiElementGameObject = CreateUIElement();
+
+        GameObjectUtility.SetParentAndAlign(uiElementGameObject, Selection.activeGameObject);
+
+        Selection.activeGameObject = uiElementGameObject;
+    }
+
+    private static GameObject CreateUIElement()
+    {
+        const string prefabPath = "Assets/Prefabs/Text Prefab.prefab";
+
+        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+        var gameObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+
+        gameObject.name = prefab.name;
+
+        Undo.RegisterCreatedObjectUndo(gameObject, $"Created {gameObject.name}");
+
+        return gameObject;
+    }
+}
