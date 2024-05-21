bae7cc8ce98a38e1b9d5b7915663f63c9ef7b212
SirDerpsAlot <57239131+SirDerpsAlot@users.noreply.github.com>
2024-05-21 12:44:36 -0400

    Added Button Prefab (#2)
    Created prefab for Elle Buttons

---
+++ cARnival-Project/Assets/Scripts/Prefab scipts/ButtonPrefabScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPrefabScript : MonoBehaviour
+{
+    public Sprite ButtonImage = null;
+    public TextPrefabScript Text;
+
+    private void OnValidate()
+    {
+        var Img = gameObject.GetComponentInChildren<Image>();
+
+        if (Img == null)
+        {
+            Img.color = Color.clear;
+        }
+        else
+        {
+            Img.color = Color.white;
+        }
+
+        Img.sprite = ButtonImage;
+    }
+
+    [MenuItem("GameObject/ELLE/Button", false)]
+    private static void Create(MenuCommand menuCommand)
+    {
+        var uiElementgameObject = CreateUIElement();
+
+        GameObjectUtility.SetParentAndAlign(uiElementgameObject, Selection.activeGameObject);
+
+        Selection.activeGameObject = uiElementgameObject;
+    }
+
+    private static GameObject CreateUIElement()
+    {
+        const string prefabPath = "Assets/Prefabs/Button Prefab.prefab"; 
+
+        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+        var gameObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+
+        gameObject.name = prefab.name;
+
+        Undo.RegisterCreatedObjectUndo(gameObject, $"Created {gameObject.name}");
+
+        return gameObject;
+    }
+}
