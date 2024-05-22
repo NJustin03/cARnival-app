681a92a642c9e95316a57ecaff1667dceaac017b
Nathan Justin <nathanjustin03@gmail.com>
2024-05-22 13:28:14 -0400

    Added Scripts for Putting an object on a AR Plane

--- cARnival-Project/Assets/Scripts/PlaneManager.cs
+++ cARnival-Project/Assets/Scripts/PlaneManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlaneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject spawnableObject;
+    private ARPlaneManager arPlaneManager;
+    private bool planeDetected = false;
+
+    private void Awake()
     {
-        
+        arPlaneManager = GetComponent<ARPlaneManager>();
+    }
+    private void OnEnable()
+    {
+        arPlaneManager.planesChanged += OnPlaneChanged;
+    }
+    private void OnDisable()
+    {
+        arPlaneManager.planesChanged -= OnPlaneChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnPlaneChanged(ARPlanesChangedEventArgs args)
     {
-        
+        if (!planeDetected && args.added.Count > 0)
+        {
+            //Processes the first plane & sets the position for objects to spawn
+            ARPlane firstPlane = args.added[0];
+            Vector3 spawnPosition = firstPlane.transform.position;
+            Instantiate(spawnableObject, spawnPosition, Quaternion.identity);
+
+            planeDetected = true;
+
+            arPlaneManager.enabled = false;
+
+            //Ensures that any planes that are possibly added to the scene are now inactive
+            foreach (var plane in arPlaneManager.trackables)
+            {
+                plane.gameObject.SetActive(false);
+            }
+
+            firstPlane.gameObject.SetActive(true);
+        }
     }
 }
