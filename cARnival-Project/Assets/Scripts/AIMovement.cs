15f8a1910c3d4ba87f10217ceb04914252ea6b6f
SkyDrqgon <nathanjustin03@gmail.com>
2024-05-27 22:33:21 -0400

    Temporary Duck Path FInding

---
+++ cARnival-Project/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+    private NavMeshAgent duckAgent;
+
+    void Start()
+    {
+        if (waypoints.Length > 0)
+        {
+            transform.position = waypoints[0].position;
+        }
+
+        duckAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        MoveTowardsWaypoint();
+    }
+
+    void MoveTowardsWaypoint()
+    {
+        if (waypoints != null)
+        {
+            Transform targetWaypoint = waypoints[currentWaypointIndex];
+            Vector3 direction = targetWaypoint.position - transform.position;
+            transform.position += direction.normalized * duckAgent.speed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.1f)
+            {
+                currentWaypointIndex = Random.Range(0, waypoints.Length - 1);
+            }
+        }
+    }
+}
