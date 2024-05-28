4d13c71689b91928b532c8f62258363516da34c2
SirDerpsAlot <57239131+SirDerpsAlot@users.noreply.github.com>
2024-05-28 17:20:56 -0400

    Fishing game backend base
    Created base scripts and functionality for the fishing game

---
+++ cARnival-Project/Assets/Scripts/GameScripts/FishingGameQuestionBoard.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishingGameQuestionBoard : MonoBehaviour
+{
+    private enum TermType
+    {
+        Word,
+        Image,
+        Audio
+    }
+    public GameObject QuestionBoard;
+
+    [SerializeField]
+    private GameObject TermWordGameObject = null;
+
+    [SerializeField]
+    private GameObject TermImageGameObject = null;
+
+    [SerializeField]
+    private GameObject TermAudioGameObject = null;
+
+    [SerializeField]
+    private TextPrefabScript TermWordText = null;
+
+    [SerializeField]
+    private Image TermImageImage = null;
+
+    public void ConfigureWithWord(string Term)
+    {
+        var allEnumValues = Enum.GetValues(typeof(TermType));
+        var filteredEnumValues = new List<TermType>();
+        
+        var randomIndex = UnityEngine.Random.Range(0, filteredEnumValues.Count() - 1);
+        var randomTermType = filteredEnumValues[randomIndex];
+
+        TermWordGameObject.SetActive(false);
+        TermImageGameObject.SetActive(false);
+        TermAudioGameObject.SetActive(false);
+
+        switch (randomTermType)
+        {
+            case TermType.Image:
+                /*
+                if (!Term.hasImage)
+                {
+                    goto case TermType.Word;
+                }
+                */
+                    TermImageGameObject.SetActive(true);
+                    // TermImageImage.sprite = // TODO: Get the image for the word and assign it
+                    break;
+                
+            case TermType.Audio:
+                /*
+                if (!Term.hasAudio)
+                {
+                    goto case TermType.Word;
+                }
+                */
+                TermAudioGameObject.SetActive(true);
+                break;
+
+            case TermType.Word:
+                TermWordGameObject.SetActive(true);
+                TermWordText.Text = Term;
+                break;
+        }
+    }
+
+    public void OnPlayAudio()
+    {
+        // TODO: Get the word Audio Clip
+        FishingGameManager.shared.PlayAudioClip(null);
+    }
+}
