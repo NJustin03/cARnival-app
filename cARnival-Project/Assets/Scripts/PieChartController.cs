52aa1b38f3d803d1ec28d883f53f529eab49fd20
Dandan0809 <daniellexu0809@gmail.com>
2024-06-12 18:01:27 -0400

    Added Stats Page & Drafts of Profile and Store

---
+++ cARnival-Project/Assets/Scripts/PieChartController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PieChartController : MonoBehaviour
+{
+    public Image fill;
+    public float score;
+    public TextMeshProUGUI num_percentage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        setValues(score);
+        setPercentage(score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setValues(float score)
+    {
+        fill.fillAmount = score;
+    }
+
+    public void setPercentage(float score)
+    {
+        float percentage = score * 100f;
+        num_percentage.text = percentage.ToString("F1") + "%";
+    }
+
+}
