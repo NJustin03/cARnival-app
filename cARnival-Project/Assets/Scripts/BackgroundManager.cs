c28b1daf46da43222201afe01c261a2fea05bb8e
Dandan0809 <daniellexu0809@gmail.com>
2024-06-25 01:02:01 -0400

    Updated Profile Page

---
+++ cARnival-Project/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public GameObject background;
+    public Button button_active;
+    public Button button_inactive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActiveBackground()
+    {
+        background.SetActive(true);
+    }
+
+    public void InactiveBackground()
+    {
+        background.SetActive(false);
+    }
+
+}
