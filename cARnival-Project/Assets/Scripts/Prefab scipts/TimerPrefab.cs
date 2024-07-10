152aec7719340e65c18685e45076da0e4ed58c6f
SkyDrqgon <nathanjustin03@gmail.com>
2024-07-09 22:19:46 -0400

    Reapply "Merge branch 'Basketball-Bankend-nathan' into Master"
    This reverts commit ae104688f9cebd7bd13d2abf1589274963cc2f02.

---
+++ cARnival-Project/Assets/Scripts/Prefab scipts/TimerPrefab.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerPrefab : MonoBehaviour
+{
+    public float timeLeft;
+    public TextPrefabScript timerText;
+
+    private bool timerOn = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerOn)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                Countdown(timeLeft);
+
+            }
+            else
+            {
+                timeLeft = 0;
+                timerOn = false;
+            }
+        } 
+    }
+
+    void Countdown(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.Text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
