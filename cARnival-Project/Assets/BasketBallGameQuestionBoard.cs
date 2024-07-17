c39b6aa9ea8113c20d9f407845dd65f7d9c77a80
SirDerpsAlot <57239131+SirDerpsAlot@users.noreply.github.com>
2024-07-17 13:22:45 -0400

    Fixed audio and images game components
    Audio and images now both appear in fishing game, and basketball game uses Audio if it exists.

---
+++ cARnival-Project/Assets/BasketBallGameQuestionBoard.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketBallGameQuestionBoard : MonoBehaviour
+{
+   
+    public GameObject QuestionBoard;
+
+    [SerializeField]
+    private GameObject TermWordGameObject = null;
+
+    [SerializeField]
+    private GameObject TermAudioGameObject = null;
+
+    [SerializeField]
+    private TextPrefabScript TermWordText = null;
+
+    private AudioClip TermAudio = null;
+
+    public void ConfigureWithWord(Answer Term)
+    {
+
+        TermWordGameObject.SetActive(false);
+        TermAudioGameObject.SetActive(false);
+   
+        switch ("Audio")
+        {
+           //TODO: add logic for setting to go to word
+            case "Audio":
+                if (Term.hasAudio)
+                {
+                    Debug.Log("No sound found!");
+                    goto case "Word";
+                }
+
+                TermAudio = Term.GetAudio();
+                TermAudioGameObject.SetActive(true);
+                OnPlayAudio();
+                break;
+
+            case "Word":
+                TermWordGameObject.SetActive(true);
+                TermWordText.Text = Term.GetFront();
+                break;
+        }
+    }
+
+    public void OnPlayAudio()
+    {
+        LanguageHoopsManager.shared.PlayAudioClip(TermAudio);
+    }
+}
