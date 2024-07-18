7893fc85cd9daf382c9dc09c348c085f3d656c75
Miguel Rosario <miguelangel53013@gmail.com>
2024-07-17 21:13:31 -0400

    Updated stats box text.

--- cARnival-Project/Assets/Scripts/Prefab scipts/UpdateStatsBoxScript.cs
+++ cARnival-Project/Assets/Scripts/Prefab scipts/UpdateStatsBoxScript.cs
@@ -6,8 +6,7 @@ public class UpdateStatsBoxScript : MonoBehaviour
 {
     public PieChartController pieChart;
     public TextPrefabScript moduleName;
-    public TextPrefabScript hourCount;
-    public TextPrefabScript minuteCount;
+    public TextPrefabScript timeCount;
 
     public void UpdateStatsBox(ModuleStatsJson stats)
     {
@@ -19,7 +18,8 @@ public class UpdateStatsBoxScript : MonoBehaviour
             string[] daysAndHours = averageSessionLength[0].Split("days,");
             averageSessionLength[0] = (int.Parse(daysAndHours[0]) * 24 + int.Parse(daysAndHours[1])).ToString();
         }
-        hourCount.Text = averageSessionLength[0];
-        minuteCount.Text = averageSessionLength[1];
+
+        timeCount.Text = averageSessionLength[0] + " hours " + averageSessionLength[1] + " minutes";
+
     }
 }
